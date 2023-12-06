d4d69494e13ae8016f4390e74fb8c21b4534a5bf
HulusiGiray <hulusiguzel02@gmail.com>
2023-12-06 21:44:42 +0300

    ders20JavaScript

--- ders20javascript/javascript.js
+++ ders20javascript/javascript.js
@@ -1,3 +1,10 @@
 function myFunction3(){
     document.getElementById("demo1").innerHTML = "External(Dış) script çalıştı"
 }
+
+// html'de 84.satırdan sonra buraya geçtik
+
+var x = 5;
+var y = 6;
+var z = x+y;
+console.log(z)//web sayfasında incele>konsol yaptığında 11'i görürsün
